010c5b5ffd84e1afb59565214bbb63f3dcf66aee
27293 <myltd6@fpt.com.vn>
2020-12-16 10:29:56 +0700

    first commit

--- app/src/main/java/com/ngodangtann/bottomnavbartutorialandroid/MainActivity.kt
+++ app/src/main/java/com/ngodangtann/bottomnavbartutorialandroid/MainActivity.kt
@@ -2,10 +2,36 @@ package com.ngodangtann.bottomnavbartutorialandroid
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.ngodangtann.bottomnavbartutorialandroid.fragments.FavoriteFragment
+import com.ngodangtann.bottomnavbartutorialandroid.fragments.HomeFragment
+import com.ngodangtann.bottomnavbartutorialandroid.fragments.SettingsFragment
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val homeFragment = HomeFragment()
+        val favoritesFragment = FavoriteFragment()
+        val settingsFragment = SettingsFragment()
+
+        makeCurrentFragment(homeFragment)
+
+        bottom_navigation.setOnNavigationItemSelectedListener {
+            when(it.itemId){
+                R.id.ic_home -> makeCurrentFragment(homeFragment)
+                R.id.ic_favorite -> makeCurrentFragment(favoritesFragment)
+                R.id.ic_settings -> makeCurrentFragment(settingsFragment)
+            }
+            true
+        }
     }
+
+    private fun makeCurrentFragment(fragment: Fragment) =
+        supportFragmentManager.beginTransaction().apply {
+            replace(R.id.fl_wrapper,fragment)
+            commit()
+        }
 }
